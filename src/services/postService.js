26b1faeed519428ae93e03227662521433a37336
LingAllie <132822814+LingAllie@users.noreply.github.com>
2024-03-31 23:23:25 +0700

    post_join_acti

--- src/services/postService.js
+++ src/services/postService.js
@@ -4,7 +4,6 @@ import axiosInstance from '../utils/axiosInstance';
 export const postService = {
     getPost() {return axiosInstance.get("/posts")},
     updatePost(postId, updatedPostData) {return axiosInstance.put(`/update_post_data`, {postId, updatedPostData})},
-    updateStdJoin(postId, stuId) {return axiosInstance.put(`/update_std_join`, {postId, stuId})}
 }
 
 export default postService;
