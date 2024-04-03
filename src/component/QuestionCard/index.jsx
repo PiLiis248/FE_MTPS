553bd20bb03c1dceaf6c8785b713beeed6e075a2
LingAllie <132822814+LingAllie@users.noreply.github.com>
2024-04-03 15:13:29 +0700

    clear_each

--- src/component/QuestionCard/index.jsx
+++ src/component/QuestionCard/index.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+// QuestionCard.js
+import React, { useEffect, useState } from 'react';
 import '../../../public/assets/css/test.css'; // Import the test.css file
 
-const QuestionCard = ({ question, index, handleChange }) => {
+const QuestionCard = ({ question, index, handleChange, counter }) => {
     const { question: questionText, options, correctAnswer } = question;
+    const [selectedOption, setSelectedOption] = useState('');
+
+    useEffect(() => {
+        const saveAnswer = localStorage.getItem(`answer-${index}`);
+        if (saveAnswer) {
+            handleChange(`question-${index}`, saveAnswer);
+            setSelectedOption(saveAnswer);
+        }
+    }, [index, handleChange]);
 
     const handleOptionChange = (event) => {
-        const { name, value } = event.target;
-        handleChange(name, value);
+        const { value } = event.target;
+        setSelectedOption(value);
+        handleChange(`question-${index}`, value);
+        localStorage.setItem(`answer-${index}`, value);
     };
+
+    const handleClearAnswer = () => {
+        setSelectedOption('');
+        handleChange(`question-${index}`, '');
+        localStorage.removeItem(`answer-${index}`);
+    }
 
     return (
         <div className="question-card">
-            <h2>{questionText}</h2>
+            <h2>{counter}. {questionText}<span className="required-star">*</span></h2>
             <div className="options">
                 {Object.entries(options).map(([key, option]) => (
                     <div key={key} className="option">
@@ -20,9 +38,15 @@ const QuestionCard = ({ question, index, handleChange }) => {
                             id={`option-${key}`}
                             name={`question-${index}`}
                             value={option.text}
+                            checked={selectedOption === option.text}
                             onChange={handleOptionChange}
                         />
-                        <label htmlFor={`option-${key}`}>{option.text}</label>
+                        <label 
+                            htmlFor={`option-${key}`} 
+                            onClick={(e) => { e.preventDefault(); e.stopPropagation(); }}
+                        >
+                            {option.text}
+                        </label>
                     </div>
                 ))}
             </div>
@@ -32,6 +56,7 @@ const QuestionCard = ({ question, index, handleChange }) => {
                 value={correctAnswer}
                 onChange={(e) => handleChange(`correct-answer-${index}`, e.target.value)}
             />
+            <button onClick={handleClearAnswer} className='clear-button'>Clear Answer</button>
         </div>
     );
 };
--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,21 +1,24 @@
+
+
+// TestPage.js
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import '../../../public/assets/css/testPage.css'; // Ensure this path is correct for your project
 import '../../../public/assets/css/test.css'; // Ensure this path is correct for your project
 import { testService } from '../../services/testService'; // Import your test service
 import QuestionCard from '../../component/QuestionCard'; // Import the QuestionCard component
+import { PATHS } from '../../constants/path';
 
 const TestPage = () => {
     const { testId } = useParams(); 
     const [currentTest, setCurrentTest] = useState(null);
+    const [answers, setAnswers] = useState({}); // State to store selected answers
 
     useEffect(() => {
         const fetchTest = async () => {
             try {
-                console.log(testId);
                 const response = await testService.getSpecificTest(testId); 
                 setCurrentTest(response.data); 
-                console.log(response.data);
             } catch (error) {
                 console.error('Error fetching test data:', error);
             }
@@ -24,10 +27,80 @@ const TestPage = () => {
         fetchTest(); 
     }, [testId]); 
 
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     // Check if all questions have been answered
+    //     if (Object.keys(answers).length === Object.keys(currentTest.questions).length) {
+    //         console.log("Form submitted");
+    //         // Save answers to local storage
+    //         Object.entries(answers).forEach(([key, value]) => {
+    //             localStorage.setItem(key, value);
+    //         });
+    //     } else {
+    //         console.log("Please answer all questions.");
+    //     }
+    // };
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        console.log("Form submitted");
+        // Check if all questions have been answered
+        if (Object.keys(answers).length === Object.keys(currentTest.questions).length) {
+            console.log("Form submitted");
+            // Save answers to local storage
+            Object.entries(answers).forEach(([key, value]) => {
+                localStorage.setItem(key, value);
+            });
+            // Notify submission
+            alert("Form submitted successfully!");
+        } else {
+            console.log("Please answer all questions.");
+            // Scroll to the first unanswered question
+            const firstUnansweredQuestionIndex = Object.keys(currentTest.questions).findIndex(
+                questionId => !answers[`question-${questionId}`]
+            );
+            if (firstUnansweredQuestionIndex !== -1) {
+                const element = document.querySelector(`#question-${firstUnansweredQuestionIndex}`);
+                window.scrollTo({
+                    top: element.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        }
     };
+    
+
+
+    const handleAnswerChange = (questionId, answer) => {
+        setAnswers({ ...answers, [questionId]: answer });
+    };
+
+    // const handleClearAll = () => {
+    //     // Clear all answers
+    //     setAnswers({});
+    //     // Clear answers from local storage
+    //     Object.keys(currentTest.questions).forEach((questionId, index) => {
+    //         localStorage.removeItem(`answer-${index}`);
+    //     });
+    //     console.log("All answers cleared.");
+    // };
+
+    const handleClearAll = () => {
+        // Clear answers from local storage
+        Object.keys(currentTest.questions).forEach((questionId, index) => {
+            localStorage.removeItem(`answer-${index}`);
+        });
+        // Reset state of selected answers for all questions
+        const newAnswers = {};
+        Object.keys(currentTest.questions).forEach((questionId) => {
+            newAnswers[`question-${questionId}`] = ''; // Set answer to empty string
+        });
+        setAnswers(newAnswers);
+        console.log("All answers cleared.");
+        // Scroll to the top of the page
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
+    
+    
 
     if (!currentTest) {
         return <p>Loading test data...</p>;
@@ -43,12 +116,18 @@ const TestPage = () => {
     return (
         <div className="test-creation-page">
             <hr />
+            <Link to={PATHS.HOME} className='back-btn'>Back Home Page</Link>
+            
             <form onSubmit={handleSubmit} className="create-questions-container">
                 {questionsArray.map((question, index) => (
-                    <QuestionCard key={index} index={index} question={question} />
+                    <QuestionCard key={index} index={index} question={question} handleChange={handleAnswerChange} counter={index+1}/>
                 ))}
-                <button type="submit" className="submit-button">Submit Test</button>
+                <div className="button">
+                    <button type="clear_all" onClick={handleClearAll} className="clear-all-button">Clear All</button>
+                    <button type="submit" className="submit-button">Submit Test</button>
+                </div>
             </form>
+            
         </div>
     );
 };
