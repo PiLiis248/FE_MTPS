6e3afc0b9d5c88bc78684554d5325b7ba00877bb
Jenny Thai <jennythai.fw@gmail.com>
2024-04-09 15:22:09 +0700

    "fix param testid"

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,117 +1,126 @@
 // TestPage.js
 
-import React, { useEffect, useState } from 'react';
-import { Link, useParams, useNavigate } from 'react-router-dom';
-import { testService } from '../../services/testService';
-import QuestionCard from '../../component/QuestionCard';
-import { PATHS } from '../../constants/path';
-import { useAuthContext } from '../../context/AuthContext';
+import React, { useEffect, useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
+import { testService } from "../../services/testService";
+import QuestionCard from "../../component/QuestionCard";
+import { PATHS } from "../../constants/path";
+import { useAuthContext } from "../../context/AuthContext";
 
 const TestPage = () => {
-    const { testId } = useParams();
-    const { profile } = useAuthContext();
-    const [currentTest, setCurrentTest] = useState(null);
-    const [answers, setAnswers] = useState({});
-    const [allAnswersCleared, setAllAnswersCleared] = useState(false);
-    const navigate = useNavigate();
+  const { testId } = useParams();
+  const { profile } = useAuthContext();
+  const [currentTest, setCurrentTest] = useState(null);
+  const [answers, setAnswers] = useState({});
+  const [allAnswersCleared, setAllAnswersCleared] = useState(false);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const fetchTest = async () => {
-            try {
-                const response = await testService.getSpecificTest(testId);
-                setCurrentTest(response.data);
-            } catch (error) {
-                console.error('Error fetching test data:', error);
-            }
-        };
-
-        fetchTest();
-    }, [testId]);
-
-    useEffect(() => {
-        // Load saved answers from localStorage
-        const savedAnswers = {};
-        Object.keys(currentTest?.questions || {}).forEach((questionId, index) => {
-            const savedAnswer = localStorage.getItem(`question_${index}`);
-            if (savedAnswer) {
-                savedAnswers[`question_${index}`] = savedAnswer;
-            }
-        });
-        setAnswers(savedAnswers);
-    }, [currentTest]);
-
-    
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-      
-        const answers = {};
-        Object.keys(currentTest?.questions || {}).forEach((questionId, index) => {
-          const savedAnswer = localStorage.getItem(`question_${index}`);
-          if (savedAnswer) {
-            answers[questionId] = savedAnswer;
-            
-          }
-        });
-        
-        
-        try {
-            console.log(answers);
-        console.log(testId);
-        console.log(profile.id);
-          const response = await testService.doTest(
-            answers,
-            testId,
-            profile.id // Add profileId to the request body
-          );
-
-          navigate(PATHS.HOME);
-          // Handle the response
-        } catch (error) {
-          console.error('Error submitting test:', error);
-        }
-      };
-
-    const handleAnswerChange = (questionId, optionId) => {
-        setAnswers(prevAnswers => ({
-            ...prevAnswers,
-            [questionId]: optionId
-        }));
-
-        localStorage.setItem(questionId, optionId);
+  useEffect(() => {
+    const fetchTest = async () => {
+      try {
+        const response = await testService.getSpecificTest(testId);
+        setCurrentTest(response.data);
+      } catch (error) {
+        console.error("Error fetching test data:", error);
+      }
     };
 
-    const handleClearAll = () => {
-        localStorage.clear();
-        window.location.reload();
-    };
+    fetchTest(testId);
+  }, [testId]);
 
-    if (!currentTest) {
-        return <p>Loading test data...</p>;
+  useEffect(() => {
+    // Load saved answers from localStorage
+    const savedAnswers = {};
+    Object.keys(currentTest?.questions || {}).forEach((questionId, index) => {
+      const savedAnswer = localStorage.getItem(`question_${index}`);
+      if (savedAnswer) {
+        savedAnswers[`question_${index}`] = savedAnswer;
+      }
+    });
+    setAnswers(savedAnswers);
+  }, [currentTest]);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const answers = {};
+    Object.keys(currentTest?.questions || {}).forEach((questionId, index) => {
+      const savedAnswer = localStorage.getItem(`question_${index}`);
+      if (savedAnswer) {
+        answers[questionId] = savedAnswer;
+      }
+    });
+
+    try {
+    //   console.log(answers);
+    //   console.log(testId);
+    //   console.log(profile.id);
+      const response = await testService.doTest(
+        answers,
+        testId,
+        profile.id // Add profileId to the request body
+      );
+
+      navigate(PATHS.HOME);
+      // Handle the response
+    } catch (error) {
+      console.error("Error submitting test:", error);
     }
+  };
 
-    return (
-        <div className="test-creation-page">
-            <hr />
-            <Link to={PATHS.HOME} className='back-btn'>Back Home Page</Link>
+  const handleAnswerChange = (questionId, optionId) => {
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [questionId]: optionId,
+    }));
 
-            <form onSubmit={handleSubmit} className="create-questions-container">
-                {Object.values(currentTest.questions).map((question, index) => (
-                    !allAnswersCleared && 
-                    <QuestionCard 
-                        key={index} 
-                        index={index} 
-                        question={question} 
-                        handleChange={handleAnswerChange} 
-                        counter={index + 1} 
-                    />
-                ))}
-                <div className="button">
-                    <button type="button" onClick={handleClearAll} className="clear-all-button">Clear All</button>
-                    <button type="submit" className="submit-button">Submit Test</button>
-                </div>
-            </form>
+    localStorage.setItem(questionId, optionId);
+  };
+
+  const handleClearAll = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
+  if (!currentTest) {
+    return <p>Loading test data...</p>;
+  }
+
+  return (
+    <div className="test-creation-page">
+      <hr />
+      <Link to={PATHS.HOME} className="back-btn">
+        Back Home Page
+      </Link>
+
+      <form onSubmit={handleSubmit} className="create-questions-container">
+        {Object.values(currentTest.questions).map(
+          (question, index) =>
+            !allAnswersCleared && (
+              <QuestionCard
+                key={index}
+                index={index}
+                question={question}
+                handleChange={handleAnswerChange}
+                counter={index + 1}
+              />
+            )
+        )}
+        <div className="button">
+          <button
+            type="button"
+            onClick={handleClearAll}
+            className="clear-all-button"
+          >
+            Clear All
+          </button>
+          <button type="submit" className="submit-button">
+            Submit Test
+          </button>
         </div>
-    );
+      </form>
+    </div>
+  );
 };
 
 export default TestPage;
--- src/services/testService.js
+++ src/services/testService.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import axiosInstance from '../utils/axiosInstance';
+import React from "react";
+import axiosInstance from "../utils/axiosInstance";
 
 export const testService = {
-    getSpecificTest(testId) {return axiosInstance.get(`/specific_test/${ testId }`)},
-    // doTest(answers, testId, profileId) {return axiosInstance.put(`/test_answer`), {answers, testId, profileId}}
-    doTest(answers, testId, profileId) {
-        return axiosInstance.put('/test_answer', {answers, testId, profileId})
-    }
-}
+  getSpecificTest(testId) {
+    return axiosInstance.get(`/specific_test?testId=${testId}`);
+  },
+  // doTest(answers, testId, profileId) {return axiosInstance.put(`/test_answer`), {answers, testId, profileId}}
+  doTest(answers, testId, profileId) {
+    return axiosInstance.put("/test_answer", { answers, testId, profileId });
+  },
+};
 
 export default testService;
