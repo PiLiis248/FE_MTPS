22a71c344873d2651cc3e8e2465f371dadc96013
Jenny Thai <jennythai.fw@gmail.com>
2024-04-10 20:40:50 +0700

    "ok"

--- src/pages/FacultyPage/index.jsx
+++ src/pages/FacultyPage/index.jsx
@@ -1,57 +1,59 @@
-import React, { useState, useEffect } from 'react';
-import { useAuthContext } from '../../context/AuthContext';
-import profileService from '../../services/profileService';
-import Sidebar from '../../component/SideBar';
-import studentService from '../../services/facultyService';
-
+import React, { useState, useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
+import profileService from "../../services/profileService";
+import Sidebar from "../../component/SideBar";
+import studentService, { facultyService } from "../../services/facultyService";
+import "../../../public/assets/css/faculty.css";
+import { List } from "antd";
 const FacultyPage = () => {
-    const { profile } = useAuthContext();
-    const [students, setStudents] = useState([]);
+  const { profile } = useAuthContext();
+  const [students, setStudents] = useState([]);
 
-    useEffect(() => {
-        const fetchStudents = async () => {
-            try {
-                // Fetch students from MongoDB
-                const response = await studentService.getAllStudents();
-                // Filter students based on facultyName
-                const filteredStudents = response.data.students.filter(student => student.facultyName === profile.name);
-                setStudents(filteredStudents);
-            } catch (error) {
-                console.error("Error fetching students:", error.message);
-            }
-        };
+  useEffect(() => {
+    const fetchStudents = async () => {
+      try {
+        // Fetch students from MongoDB
+        const response = await facultyService.getAllStudents();
+        const filteredStudents = response?.data.formattedStudents.filter(
+          (student) => student.facultyName === profile.name
+        );
+        setStudents(filteredStudents);
+      } catch (error) {
+        console.error("Error fetching students:", error.message);
+      }
+    };
 
-        if (profile.role === "faculty") {
-            fetchStudents();
-        }
-    }, [profile]);
-
-    return (
-        <div>
-            <Sidebar/>
-            <h1>List of Students</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Training Points</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {students.map(student => (
-                        <tr key={student.id}>
-                            <td>{student.id}</td>
-                            <td>{student.name}</td>
-                            <td>{student.email}</td>
-                            <td>{student.trainingPoint}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+    if (profile.role === "faculty") {
+      fetchStudents();
+    }
+  }, []);
+  //   console.log(students);
+  return (
+    <div className="faculty-page">
+      <Sidebar />
+      <h1 className="title-page">List of Students</h1>
+      <div className="list-student">
+        <div className="group-title">
+          <div className="group-title-name">Name & Email</div>
+          <div className="group-title-score">Training point</div>
         </div>
-    );
+        <List
+          itemLayout="horizontal"
+          dataSource={students}
+          renderItem={(item, index) => (
+            <List.Item>
+              <List.Item.Meta
+                className="info"
+                title={item.name}
+                description={item.email}
+              />
+              <div className="training-point">{item.trainingPoint}</div>
+            </List.Item>
+          )}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default FacultyPage;
